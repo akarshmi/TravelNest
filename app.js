6b0c39d78a88c8777971db07dae6009a95333ae8
KawaljotSingh <kawaljotsinghtaneja23@gmail.com>
2026-04-01 09:10:42 +0530

    Routing for index pages and some changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express= require("express");
 const app= express();
 const mongoose= require("mongoose");
+const Listing=require("./models/listing.js");
+const path=require("path");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/travelnest";
 
@@ -14,9 +16,31 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view enjine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+// app.get("/testlisting",async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title: "My Villa",
+//         description: "Coolest Villa ever",
+//         price:2999,
+//         location:"Goa",
+//         country:"India",
+//     })
+//     await sampleListing.save();
+//     console.log("Sample saved");
+//     res.send("Successful");
+// })
+
+
 app.get("/",(req,res)=>{
     res.send("hey");
 });
+
+app.get("/listings",async (req,res)=>{
+    const allListings=await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+})
 
 app.listen(8080,()=>{
     console.log("Server is listening to port 8080");
